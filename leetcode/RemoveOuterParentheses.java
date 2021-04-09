9cdf810703a4e0607ac0d8fd79e07be8ea6571c1
Armen Harutyunov <armen.harutyunov@gmail.com>
2021-04-09 19:58:18 +0400

    1021

---
+++ leetcode/RemoveOuterParentheses.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class RemoveOuterParentheses {
+    public String removeOuterParentheses(String S) {
+        StringBuilder sb = new StringBuilder();
+        int count = 0;
+        for (char c : S.toCharArray()) {
+            if (c == '(' && count++ > 0) {
+                sb.append(c);
+            }
+            if (c == ')' && count-- > 1) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
+
